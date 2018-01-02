e4a0b0b57edb4495db5ec5c2c4de253627cca13b
Juliano Gouveia <jgouveiadev@gmail.com>
2018-01-02 11:19:49 -0800

    First commit

---
+++ demo/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import include, re_path, path
+from django.conf import settings
+
+from two_factor.urls import urlpatterns as tf_urls
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+
+# Carrega dinamicamente as URLs dos pacotes pertencentes a exchange 
+for app_name in settings.INSTALLED_APPS:
+	if app_name.startswith('exchange_'):
+		urls_path = app_name + '.urls'
+		urlpatterns.append(path('', include(urls_path)))
