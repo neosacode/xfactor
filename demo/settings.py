a4108d6f1f8a05f52b84179e94a613cd0db376b3
Juliano Gouveia <juliano@neosacode.com>
2018-01-25 17:35:36 -0200

    Config test

--- demo/settings.py
+++ demo/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=config.boolean)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=config.list)
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
