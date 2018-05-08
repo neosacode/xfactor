e291bc0bc96acf2244c3d40e6ee949009d972bf0
Juliano Gouveia <juliano@neosacode.com>
2018-05-08 13:10:55 -0300

    Add checking and investment accounts to project context_processors

---
+++ apps/investment/context_processors.py
@@ -0,0 +1,13 @@
+from exchange_core.models import Accounts, Currencies
+
+
+def accounts(request):
+	checking_accounts = Accounts.objects.filter(user=request.user, currency__symbol='BTC', currency__type=Currencies.TYPES.checking)
+	investment_accounts = Accounts.objects.filter(user=request.user, currency__symbol='BTC', currency__type=Currencies.TYPES.investment)
+	checking_account = checking_accounts.first() if checking_accounts.exists() else None
+	investment_account = investment_accounts.first() if investment_accounts.exists() else None
+
+	return {
+		'user_checking_account': checking_account,
+		'user_investment_account': investment_account
+	}
