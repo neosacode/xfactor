8c9d522e548f5e8f9c1a81adaad650343e31443a
Juliano Gouveia <juliano@neosacode.com>
2018-05-28 13:53:31 -0300

    Add static files manually

---
+++ staticfiles/includes/organisms/menu-retratil.php
@@ -0,0 +1,12 @@
+<div class="back-menu-2"id="item1">
+	<div class="container">
+		<div class="menu-2">
+			<ul>
+				<li><a href=""><img src="img/logo-6.png" alt=""><span>Carteiras</span></a></li>
+				<li><a href=""><img src="img/logo-7.png" alt=""><span>Extratos</span></a></li>
+				<li><a href=""><img src="img/logo-8.png" alt=""><span>Pay Bill</span></a></li>
+				<li><a href=""><img src="img/logo-9.png" alt=""><span>Phone Recharge</span></a></li>
+			</ul>
+		</div>
+	</div>
+</div>
