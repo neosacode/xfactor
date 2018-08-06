705e08afa4e61ede0628adabe20f21f64330e342
Juliano Gouveia <juliano@neosacode.com>
2018-08-06 10:04:00 -0300

    Add bootstrap pagination

--- xfactor/settings.py
+++ xfactor/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap_pagination',
 
     'exchange_core',
     'exchange_payments',
