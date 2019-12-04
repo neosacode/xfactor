7a7719db72ac4c033f9d0b5227966baf0cd8a8a0
Zerando <zerando@protonmail.com>
2019-12-04 20:29:26 -0300

    Remove configuration from prettyconf

--- apps/investment/__init__.py
+++ apps/investment/__init__.py
@@ -1,14 +1,11 @@
 from decimal import Decimal
 
 from django.conf import settings
-from prettyconf import Configuration
+from prettyconf import config
 
 
 # Tells to Django where it is the configuration class of this module
 default_app_config = 'apps.investment.apps.Config'
-
-# Prettyconf
-config = Configuration(starting_path=settings.BASE_DIR)
 
 # Credit line config
 settings.LOAN_INTEREST_PERCENT = config('LOAN_INTEREST_PERCENT', cast=Decimal, default=Decimal('30'))
